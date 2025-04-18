9c78e1c196ef4018bf0c54224e1e6e1419137135
Pawankashap <126440752+Pawankashap@users.noreply.github.com>
2025-04-17 23:34:03 -0700

    add test const

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting=`Welcome, ${currentUser.slice(0, 1)}!`;
+const test="test name";
 
